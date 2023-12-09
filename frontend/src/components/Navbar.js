642accccb77e97ccd55268bb1d0f9c61852fe723
Nick Strauch <njs35@duke.edu>
2023-12-09 17:41:55 -0500

    Updated link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <nav className="navbar">
             <AiOutlineBars size="35px" onClick={() => handleMenuClick()} style={{ cursor: 'pointer' }} className="navbar-menu"/>  
             <h1 className="navbar-title">FixIt</h1>
-            <a href="https://github.com/NickStrauch13/generative-ai-hackathon"><AiFillGithub size="35px" className="navbar-github"/></a>
+            <a href="https://github.com/NickStrauch13/IDS706-Individual-Project-4"><AiFillGithub size="35px" className="navbar-github"/></a>
             
         </nav>
     )
