f1d1fbcb7bba065eab4f8aaa8d7ffb7c63350258
Nick Strauch <njs35@duke.edu>
2023-12-09 18:58:58 -0500

    tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -4,20 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def test_generate_steps():
-    """
-    Tests the generate_steps endpoint.
-    """
-    # Get testing url
-    url = "http://iproject4-flask.azurewebsites.net"
-    url += "/generate_steps"
-    body = {"query": "My dishwasher is broken, what should I do?"}
-    # Make request
-    response = requests.post(url, json=body)
-    # Check status code
-    assert response.status_code == 200
-
-
 def test_health():
     """
     Tests the health endpoint.
