8a7af779d3175e68b65a482a71e9e13cd13aad76
Nick Strauch <njs35@duke.edu>
2023-12-09 18:48:01 -0500

    updated tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,21 +1,32 @@
-from combined_yt_gpt import main_combined, get_difficulty_and_time
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-def test_main_combined():
+def test_generate_steps():
     """
-    Test main_combined function.
-    Checks to verify that the function returns steps and a youtube link.
+    Tests the generate_steps endpoint.
     """
-    steps, youtube_link = main_combined()
-    assert steps
-    assert youtube_link
+    # Get testing url
+    url = os.getenv("TEST_URL")
+    body = {"query": "My dishwasher is broken, what should I do?"}
+    # Make request
+    response = requests.post(url, json=body)
+    # Check status code
+    assert response.status_code == 200
 
 
-def test_get_difficulty_and_time():
+def test_health():
     """
-    Test get_difficulty_and_time function.
-    Checks to verify valid time and difficulty are returned.
+    Tests the health endpoint.
     """
-    difficulty, time = get_difficulty_and_time()
-    assert difficulty
-    assert time
+    # Get testing url
+    url = os.getenv("TEST_URL")
+    url += "/example"
+    # Make request
+    response = requests.get(url)
+    # Check status code
+    assert response.status_code == 200
+
+
